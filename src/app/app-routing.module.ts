753aff2b3508dad52dd92c63ef1e214627423ec2
Rajib <saidul.bd@asa-international.com>
2023-06-17 13:31:52 +0600

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  { path:'', component: HomeComponent},
-  { path:'home', component: HomeComponent},
+  { path:'', component: PagesComponent},
+  { path:'home', component: PagesComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { IonicModule } from '@ionic/angular';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    PagesComponent,
+    
   ],
   imports: [
     BrowserModule,
