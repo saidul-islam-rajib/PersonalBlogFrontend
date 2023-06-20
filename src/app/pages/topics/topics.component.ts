0d2ddd9d47a5411fdc8c4f3eaeb18aa7c770e946
Rajib <saidul.bd@asa-international.com>
2023-06-20 22:13:54 +0600

    Fix navigate to top button issue in "app.component.ts" file

--- src/app/pages/topics/topics.component.ts
+++ src/app/pages/topics/topics.component.ts
@@ -7,53 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicsComponent implements OnInit {
 
-  ngOnInit() {
-    /**
-     * Add event listener on multiple elements
-     */
-  
-    const addEventOnElements = function(elements: NodeListOf<Element>, eventType: string, callback: EventListenerOrEventListenerObject) {
-      for (let i = 0, len = elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType, callback);
-      }
-    }
-  
-    /**
-     * MOBILE NAVBAR TOGGLER
-     */
-  
-    const navbar = document.querySelector("[data-navbar]") as HTMLElement;
-    const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-  
-    const toggleNav = () => {
-      navbar.classList.toggle("active");
-      document.body.classList.toggle("nav-active");
-    }
-  
-    addEventOnElements(navTogglers, "click", toggleNav);
-  
-    /**
-     * HEADER ANIMATION
-     * When scrolled down to 100px header will be active
-     */
-  
-    const header = document.querySelector("[data-header]") as HTMLElement;
-    const backTopBtn = document.querySelector("[data-back-top-btn]") as HTMLElement;
-  
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        header.classList.add("active");
-        backTopBtn.classList.add("active");
-      } else {
-        header.classList.remove("active");
-        backTopBtn.classList.remove("active");
-      }
-    });
-  
+  ngOnInit() { 
+     
     /**
      * SLIDER
      */
-  
+
     const slider = document.querySelector("[data-slider]") as HTMLElement;
     const sliderContainer = document.querySelector("[data-slider-container]") as HTMLElement;
     const sliderPrevBtn = document.querySelector("[data-slider-prev]") as HTMLElement;
