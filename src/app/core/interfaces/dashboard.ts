2d9701aebfb3edcbce7976516f39f481e2f55e6e
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-11 23:54:04 +0600

    Format document

--- src/app/core/interfaces/dashboard.ts
+++ src/app/core/interfaces/dashboard.ts
@@ -3,6 +3,11 @@ export interface Dashboard {
   postTitle: string;
   postAbstract: string;
   userId: string;
+  topicIds: Array<{
+    topicId: string;
+    topicTitle: string;
+    userId: string;
+  }>;
   createdDateTime: Date;
   updatedDateTime: Date
 }
