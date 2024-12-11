c29d498f5327397f197178ebc2f8388811167d48
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-12 00:55:03 +0600

    Fetch post by their id

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Post } from '../interfaces/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private postUrl = `${environment.apiUrl}/post/3F13F2DB-D444-4901-B973-5B273965E425`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getPostById(postId: string): Observable<Post> {
+    console.log("post id : ", postId);
+
+    const data_url = `${this.postUrl}`;
+    let response = this.http.get<Post>(data_url);
+    return response;
+  }
 }
--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,24 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Post } from 'src/app/core/interfaces/post';
+import { PostService } from 'src/app/core/services/post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
-  posts: Post[] = [];
+  post: Post | null = null;
   showInputField = false;
   inputControl: any;
 
+  constructor(private postService: PostService) {}
+
   ngOnInit() {
-    console.log("something else have been called")
+    console.log('post id is called!');
+
+    const postId = '6759d45f-f404-800d-b38e-330d8cd0eab0';
+    this.postService.getPostById(postId).subscribe({
+      next: (data) => {
+        this.post = data;
+        console.log('POST DATA:', this.post);
+      },
+      error: (err) => {
+        console.log('ERROR IN FETCHING POST DATA : ', err);
+      },
+    });
+
     this.inputControl = new FormControl('');
   }
 
   replaceWithInput() {
     this.showInputField = true;
   }
-
 }
